1c76fc847b1df77942b015100840fbf1e286e3c0
Manvi Gupta <manvi.official1212@gmail.com>
2026-07-02 02:03:06 +0530

    Format output of marks enumeration with index and mark details

--- Day_42/enumerate.py
+++ Day_42/enumerate.py
@@ -1,5 +1,5 @@
 marks = [12,33,78,98,5,1,3,7]
 for index , mark in enumerate(marks):
-    print (index , mark)
+    print (f"Index: {index}, Mark: {mark}")
     if (index == 5):
         print("You have reached the 5th index")
