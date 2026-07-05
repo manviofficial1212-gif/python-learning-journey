a095b7406cea402c388d8d5c2058c5dd7f286dd3
Manvi Gupta <manvi.official1212@gmail.com>
2026-07-05 23:35:34 +0530

    Refactor encoding and decoding logic for improved readability and structure

--- Day_47/secret code.py
+++ Day_47/secret code.py
@@ -2,21 +2,28 @@ choice = input("Enter 'code' to encode or 'decode' to decode: ")
 message = input("Enter a message: ")
 words = message.split()
 result = []
-for word in words :
-    if choice == "code":
-        if len(word)>=3:
-            r1 = input("Enter a random string to add at the beginning: ")
-            r2 = input("Enter a random string to add at the end: ")
-            print(r1 + word[1:] + word[0] +r2 )
-        else :
-            print(word[::-1])
-    elif choice == "decode":
-        if len(word)>=3 :
-            word = word[3:]
-            word = word[:-3]
-            word = word[-1] + word[:-1]
-            print(word)
-        else :
-            print(word[::-1])
-    else :
+
+if choice == "code":
+        r1 = input("Enter a random string to add at the beginning: ")
+        r2 = input("Enter a random string to add at the end: ")
+        for word in words :
+            if len(word)>=3:
+        
+                encoded_word = (r1 + word[1:] + word[0] +r2 )
+                result.append(encoded_word)
+            else :
+                result.append(word[::-1])
+        print(" ".join(result))
+elif choice == "decode":
+        for word in words :
+            if len(word)>=3 :
+                word = word[3:]
+                word = word[:-3]
+                word = word[-1] + word[:-1]
+                result.append(word)
+            else :
+                result.append(word[::-1])
+            print(" ".join(result))
+else :
         print("Invalid choice. Please enter 'code' or 'decode'.")
+    
