d7f22651a00dee05015cacaab8a42733b3827956
Manvi Gupta <manvi.official1212@gmail.com>
2026-07-24 17:26:12 +0530

    intro to OOPs

---
+++ day_56/oops.py
@@ -0,0 +1,11 @@
+class laptop:
+    pass
+lap1 = laptop()
+lap1.brand = "Dell"
+lap1.color = "Black"
+lap2= laptop()
+lap2.brand = "HP"
+lap2.color = "Silver"
+print(lap1.brand, lap1.color)
+print(lap2.brand, lap2.color)
+    
