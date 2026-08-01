321c082ea5fa2f3d9e2f0b4a816449e5b35ed1bf
Manvi Gupta <manvi.official1212@gmail.com>
2026-08-01 17:04:26 +0530

    Static Methods in Python

---
+++ day_65/static.py
@@ -0,0 +1,13 @@
+class math:
+    @staticmethod
+    def add(a, b):
+        return a + b
+    print("Addition:", add(5, 3))
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+    print("Subtraction:", subtract(5, 3))
+    @staticmethod
+    def square(a):
+        return a * a
+    print("Square:", square(5))
