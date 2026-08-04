02ce2dd7dcccc0eb5fce01c2e86bcbfe46ae2c91
Manvi Gupta <manvi.official1212@gmail.com>
2026-08-04 16:00:29 +0530

    Add student class with class method to change school

---
+++ day_69/class.py
@@ -0,0 +1,7 @@
+class student :
+    school = "ABC School"
+    @classmethod
+    def change_school(cls, new_school):
+        cls.school = new_school
+student.change_school("XYZ School")
+print(student.school)  
